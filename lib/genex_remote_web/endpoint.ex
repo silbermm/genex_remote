9999dd66144910f905853a8cfb87b75d40014be0
Matt <matt@silbernagel.dev>
2023-01-02 18:59:38 -0500

    format

--- lib/genex_remote_web/endpoint.ex
+++ lib/genex_remote_web/endpoint.ex
@@ -35,7 +35,6 @@ defmodule GenexRemoteWeb.Endpoint do
     param_key: "request_logger",
     cookie_key: "request_logger"
 
-
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
